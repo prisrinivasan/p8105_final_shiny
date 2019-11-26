63dfcac12a3d3e5f78c1c5f4bdff83aa6f0340d2
Priyanka Srinivasan <priyankasrinivasan@Priyankas-MacBook-Air.local>
2019-11-26 15:59:34 -0500

    first import, creating shiny app with first map

---
+++ roach_shiny/app.R
@@ -0,0 +1,6 @@
+rsconnect::setAccountInfo(name = 'prisrinivasan',
+                          token = 'F736CD4CDF0F6995DA2DC4E100A311BE',
+                          secret = 'e4xCam6qJm7OMBZWKETw+27eSiDIxoGbTaeQeRdR')
+
+library(rsconnect)
+rsconnect::deployApp('./roach_shiny.Rmd')
